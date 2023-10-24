7de20fb8fc5917eaff90165795ed505783cc8bc4
Maxime Koyamba <mkoyamba@Maximes-iMac.local>
2023-10-24 16:32:05 +0200

    Tue 24 Oct 2023 16:32:05 CEST ->

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -30,14 +30,10 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 					--admin_password=$WP_ADMIN_PASSWORD \
 					--admin_email=$WP_ADMIN_EMAIL \
 					--skip-email \
-					--allow-root
 
 	wp user create  $WP_USER \
 					$WP_USER_EMAIL \
 					--user_pass=$WP_USER_PASSWORD \
-					--allow-root
-
-	wp theme install inspiro --activate --allow-root
 
 fi
 
