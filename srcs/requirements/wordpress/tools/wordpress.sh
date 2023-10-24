efb090df311c9aaaaad57f12ce1753693763682e
Maxime Koyamba <mkoyamba@Maximes-iMac.local>
2023-10-24 16:28:18 +0200

    Tue 24 Oct 2023 16:28:18 CEST ->

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -30,10 +30,12 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 					--admin_password=$WP_ADMIN_PASSWORD \
 					--admin_email=$WP_ADMIN_EMAIL \
 					--skip-email
+					--allow-root
 
 	wp user create  $WP_USER \
 					$WP_USER_EMAIL \
 					--user_pass=$WP_USER_PASSWORD
+					--allow-root
 
 fi
 
